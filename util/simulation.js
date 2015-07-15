9889fbad1d3ff59ecfc05d08a91650a25294df00
Galen Knapp <galenknapp@gmail.com>
2015-07-14 23:25:34 -0700

    Adding a utility for simulation

---
+++ util/simulation.js
@@ -0,0 +1,18 @@
+
+/**
+ * Tools for simulations
+ * @exports util/simulation
+ */
+var simulation = {
+
+  /**
+   * @param {Array.<number>} lineup 
+   * @returns {number} The average runs scored with this lineup after simulating
+   *   the number of games defined in the global config.
+   */
+  simulateGame: function(lineup) {
+
+  }
+}
+
+module.exports = simulation;
